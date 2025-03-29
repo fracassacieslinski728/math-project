1b2ece3737a0a894871560ed4533b92b8caa7f5d
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-03-29 19:14:32 +0000

    Add 22.js

---
+++ src/22.js
@@ -0,0 +1,7 @@
+// This is an example of how you might use JavaScript to create and manipulate data.
+// You can customize this to fit your specific needs or project.
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log(addNumbers(2, 3)); // Output: 5

3bce7b8f402369e1b46f1ac68280cae63df8e47d
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-03 13:17:08 +0000

    Add 27.js

---
+++ src/27.js
@@ -0,0 +1,13 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+console.log(addNumbers(5, 3)); // Output: 8
+
+const square = (num) => num * num;
+
+console.log(square(4)); // Output: 16

4c5a190acd681560737cbfc672b529c6413bd94e
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-05 14:27:33 +0000

    Add 123.js

---
+++ src/123.js
@@ -0,0 +1,14 @@
+// Example 1: Console Log Function
+console.log("Hello World!");
+
+// Example 2: Math Function
+function square(x) {
+    return x * x;
+}
+
+square(5); // Output: 25
+
+// Example 3: For Loop
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}

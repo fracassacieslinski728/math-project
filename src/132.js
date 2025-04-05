bf0f00a290ec3a1234fd5a7438d7b56cbf8b265f
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-05 15:09:37 +0000

    Add 132.js

---
+++ src/132.js
@@ -0,0 +1,13 @@
+// Example of an unoptimized JavaScript code
+
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+console.log(sumNumbers(2, 3)); // Output: 5
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiplyNumbers(4, 6)); // Output: 24

17d5b65cc57ba3458ee08e63f91000c30d99ecec
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-05 08:36:14 +0000

    Add 51.js

---
+++ src/51.js
@@ -0,0 +1,9 @@
+// This is an example of a basic function to calculate the area of a rectangle.
+function calculateArea(width, height) {
+    return width * height;
+}
+
+// Example usage:
+var width = 10;
+var height = 5;
+console.log(calculateArea(width, height)); // Output: 50

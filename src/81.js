65078eb000c9fc60bc71e25cd5bfb8982a14d0b4
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-05 11:02:31 +0000

    Add 81.js

---
+++ src/81.js
@@ -0,0 +1,8 @@
+// Example JavaScript code for generating a random number between 1 and 10
+
+function getRandomNumber() {
+    const random = Math.floor(Math.random() * 10);
+    return random;
+}
+
+console.log(getRandomNumber());

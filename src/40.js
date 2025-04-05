3db9847718216cac6867da67fdd9840d3859a445
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-05 07:44:22 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,9 @@
+// This is a simple JavaScript code snippet demonstrating basic arithmetic operations.
+let num1 = 5;
+let num2 = 3;
+
+console.log(num1 + num2); // Output: 8
+
+let sum = num1 + num2;
+
+console.log(sum);

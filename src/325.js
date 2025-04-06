f9350183e7b78a4e98ff35e03226289fd100d911
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-06 07:29:36 +0000

    Add 325.js

---
+++ src/325.js
@@ -0,0 +1,8 @@
+// Define some variables
+let num1 = 5;
+let num2 = 3;
+
+// Perform an arithmetic operation
+let result = num1 + num2;
+
+console.log("The sum of " + num1 + " and " + num2 + " is: " + result);

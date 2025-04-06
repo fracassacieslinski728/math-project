678cbff3d9db064cb794c0b5e59aea33831437da
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-06 07:46:24 +0000

    Add 328.js

---
+++ src/328.js
@@ -0,0 +1,11 @@
+// Example: A simple function to calculate the area of a rectangle
+
+function calculateArea(length, width) {
+  return length * width;
+}
+
+// Another example with different parameters
+function calculateVolume(height, baseArea) {
+  let volume = height * baseArea;
+  console.log(`The volume of the rectangular prism is ${volume.toFixed(2)}`);
+}

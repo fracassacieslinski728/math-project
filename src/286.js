ae5d283c112d74415bbef01bb7e2d85219851813
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-06 03:54:10 +0000

    Add 286.js

---
+++ src/286.js
@@ -0,0 +1,9 @@
+// Define variables
+let x = 5;
+let y = 10;
+
+// Calculate product
+let product = x * y;
+
+// Display result
+console.log("The product of " + x + " and " + y + " is: " + product);

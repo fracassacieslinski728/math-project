8210b9c7aea43e35b9bab1d362e528f9f5e19514
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-06 00:31:13 +0000

    Add 248.js

---
+++ src/248.js
@@ -0,0 +1,9 @@
+// Example of a simple JavaScript function to multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// Example of a simple JavaScript function to add two numbers
+function add(a, b) {
+  return a + b;
+}

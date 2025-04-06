4c6c19bf9f60e36661bfbb326f0c1fb57ce3f960
fracassacieslinski728 <fracassacieslinski728@gmail.com>
2025-04-06 09:20:17 +0000

    Add 346.js

---
+++ src/346.js
@@ -0,0 +1,9 @@
+// This is just an example of how you can write simple JavaScript code.
+console.log("Hello, World!");
+
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add(2, 3);
+console.log(sum); // Output: 5
